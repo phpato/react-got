0b9c01546e0ff84ec4c8717e3be07255cd89c43a
patricio <patriciocabreravera@gmail.com>
2019-08-07 02:22:42 -0400

    Updates

---
+++ precache-manifest.f087cb18e919a1426cfc1ee5e4155cc7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54c3427ceab0cffc3e0f05979bf53f82",
+    "url": "/react-got/index.html"
+  },
+  {
+    "revision": "6ea7c8d469c1fc373633",
+    "url": "/react-got/static/css/2.d4b275d9.chunk.css"
+  },
+  {
+    "revision": "b3046e6bf8c242373815",
+    "url": "/react-got/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "6ea7c8d469c1fc373633",
+    "url": "/react-got/static/js/2.99dbc429.chunk.js"
+  },
+  {
+    "revision": "b3046e6bf8c242373815",
+    "url": "/react-got/static/js/main.07571b0e.chunk.js"
+  },
+  {
+    "revision": "7ac7f17be52426246b66",
+    "url": "/react-got/static/js/runtime~main.bd00fb13.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-got/static/media/logo.5d5d9eef.svg"
+  }
+]);
